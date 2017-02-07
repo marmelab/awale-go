7a265d60e03f39f8c878f8080e64dfeb7379c9bb
zyhou <richard.maxime9@gmail.com>
2017-02-07 12:24:09 +0100

    add test game convert position player

--- src/game/game_test.go
+++ src/game/game_test.go
@@ -13,3 +13,38 @@ func TestNewGameReturn0InCurrentPlayerIndex(t *testing.T) {
 		t.Error("New Game doesn't return expected CurrentPlayerIndex")
 	}
 }
+
+func TestConvertPlayerPositionFromPlayerOneShouldReturn0(t *testing.T) {
+	position := ConvertPlayerPosition(1, 0)
+	if position != 0 {
+		t.Error("Convert player position doesn't return 0")
+	}
+}
+
+func TestConvertPlayerPositionFromPlayerOneShouldReturn5(t *testing.T) {
+	position := ConvertPlayerPosition(6, 0)
+	if position != 5 {
+		t.Error("Convert player position doesn't return 5")
+	}
+}
+
+func TestConvertPlayerPositionFromPlayerTwoShouldReturn1(t *testing.T) {
+	position := ConvertPlayerPosition(11, 1)
+	if position != 1 {
+		t.Error("Convert player position doesn't return 1")
+	}
+}
+
+func TestConvertPlayerPositionFromPlayerTwoShouldReturn6(t *testing.T) {
+	position := ConvertPlayerPosition(6, 1)
+	if position != 6 {
+		t.Error("Convert player position doesn't return 6")
+	}
+}
+
+func TestConvertPlayerPositionFromPlayerTwoShouldReturn9(t *testing.T) {
+	position := ConvertPlayerPosition(3, 1)
+	if position != 9 {
+		t.Error("Convert player position doesn't return 9")
+	}
+}
