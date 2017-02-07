02bdec16166961f9a18ae9ce79c90bf15aa1d8f9
zyhou <richard.maxime9@gmail.com>
2017-02-07 16:39:40 +0100

    add test render score

--- src/render/render_test.go
+++ src/render/render_test.go
@@ -20,3 +20,13 @@ func TestCurrentPlayerIndicatorReturnEmptySelector(t *testing.T) {
 		t.Error("Indicator doesn't return arrow")
 	}
 }
+
+func TestRenderScoreShouldReturnScoreFormat(t *testing.T) {
+	score := [2]int{0, 5}
+	expectedRender := "Score:\tPlayer (1): 0\tPlayer (2): 5\n"
+
+	render := RenderScore(score)
+	if render != expectedRender {
+		t.Error("Render score doesn't return expected render")
+	}
+}
