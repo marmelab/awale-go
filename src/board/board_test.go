354e000a6d14ec86b904f57c04bbb1f2565cbd9f
zyhou <richard.maxime9@gmail.com>
2017-02-07 11:13:09 +0100

    add new test board

--- src/board/board_test.go
+++ src/board/board_test.go
@@ -1,6 +1,7 @@
 package board
 
 import (
+	"player"
 	"reflect"
 	"testing"
 )
@@ -21,5 +22,96 @@ func TestNewBoardShouldReturnSizeErrors(t *testing.T) {
 
 	if errors == nil {
 		t.Error("New Board should return an error with an invalid board size")
+	}
+}
+
+func TestIsPickPossibleForPostionZeroShouldReturnPickPossible(t *testing.T) {
+	board, _ := New(12, 4)
+	board[0] = 2
+	playerTwo := player.New(1, true, 12)
+	if !IsPickPossible(board, playerTwo, 0) {
+		t.Error("Pick possible should return true for player two")
+	}
+}
+
+func TestIsPickPossibleForPostionZeroShouldReturnPickImpossible(t *testing.T) {
+	board, _ := New(12, 4)
+	board[0] = 2
+	playerOne := player.New(0, true, 12)
+	if IsPickPossible(board, playerOne, 0) {
+		t.Error("Pick possible should return false for player one")
+	}
+}
+
+func TestGetWinnerForMaxScoreShouldReturnPlayerOne(t *testing.T) {
+	board, _ := New(12, 4)
+	playerOne := player.New(0, true, 12)
+	var score [2]int
+	score[0] = 48
+	score[1] = 0
+	if GetWinner(playerOne, board, score) != 0 {
+		t.Error("Get Winner should return player one")
+	}
+}
+
+func TestGetWinnerForZeroScoreShouldReturnPlayerOne(t *testing.T) {
+	board, _ := New(12, 4)
+	playerTwo := player.New(1, true, 12)
+	var score [2]int
+	score[0] = 48
+	score[1] = 0
+	if GetWinner(playerTwo, board, score) != 0 {
+		t.Error("Get Winner should return player one")
+	}
+}
+
+func TestGetWinnerForContinueGameShouldReturnNoWinner(t *testing.T) {
+	board, _ := New(12, 4)
+	playerTwo := player.New(1, true, 12)
+	var score [2]int
+	score[0] = 20
+	score[1] = 10
+	if GetWinner(playerTwo, board, score) != -2 {
+		t.Error("Get Winner should return continue game")
+	}
+}
+
+func TestIsStarvingForEmptySideShouldReturnTrue(t *testing.T) {
+	board, _ := New(12, 4)
+	playerOne := player.New(0, true, 12)
+
+	for i := 6; i <= 11; i++ {
+		board[i] = 0
+	}
+
+	if !IsStarving(board, playerOne) {
+		t.Error("Is starving should return starving")
+	}
+}
+
+func TestIsStarvingForNewBoardShouldReturnFalse(t *testing.T) {
+	board, _ := New(12, 4)
+	playerTwo := player.New(1, true, 12)
+
+	if IsStarving(board, playerTwo) {
+		t.Error("Is starving should return no starving")
+	}
+}
+
+func TestIsStarvingForEmptyBoardShouldReturnTrue(t *testing.T) {
+	board, _ := New(12, 0)
+	playerTwo := player.New(1, true, 12)
+
+	if !IsStarving(board, playerTwo) {
+		t.Error("Is starving should return starving")
+	}
+}
+
+func TestCanFeedPlayerForNewBoardSouldReturnTrue(t *testing.T) {
+	board, _ := New(12, 4)
+	playerOne := player.New(0, true, 12)
+
+	if !CanFeedPlayer(playerOne, board) {
+		t.Error("Can feed player should return true")
 	}
 }
