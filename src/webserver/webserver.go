bc7ce6b1d04dfa62745256bc88c55b910fc344d2
zyhou <richard.maxime9@gmail.com>
2017-02-19 22:11:57 +0100

    return two response, player and ia game

--- src/webserver/webserver.go
+++ src/webserver/webserver.go
@@ -7,13 +7,18 @@ import (
 	"board"
 	"constants"
 	"player"
+	"ai"
 	"strconv"
 )
 
 type PositionStruct struct {
 	Position string
-	Ia       string
 	Board    board.Board
+}
+
+type AwaleStruct struct {
+	Player game.Game
+	IA     game.Game
 }
 
 func main() {
@@ -38,18 +43,28 @@ func awale(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	position, _ := strconv.Atoi(positionStruct.Position)
-	isIA, _ := strconv.Atoi(positionStruct.Ia)
 
 	playerOne := player.New(0, true, constants.PIT_COUNT)
 	playerTwo := player.New(1, false, constants.PIT_COUNT)
 	currentGame := game.New([]player.Player{playerOne, playerTwo})
 	currentGame.Board = positionStruct.Board
-	currentGame.CurrentPlayerIndex = isIA
+	currentGame.CurrentPlayerIndex = 0
 
-	currentGame = game.PlayTurn(currentGame, game.ConvertPlayerPosition(position, currentGame.CurrentPlayerIndex))
+	gamePlayer := game.PlayTurn(currentGame, game.ConvertPlayerPosition(position, currentGame.CurrentPlayerIndex))
+	gamePlayer = game.CheckWinner(gamePlayer)
+	gamePlayer = game.SwitchPlayer(gamePlayer)
+
+	position, _ = ai.GetPosition(gamePlayer)
+	gameIA := game.PlayTurn(gamePlayer, position)
+	gameIA = game.CheckWinner(gameIA)
+	gameIA = game.SwitchPlayer(gameIA)
+
+	var awale AwaleStruct
+	awale.Player = gamePlayer
+	awale.IA = gameIA
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(currentGame)
+	json.NewEncoder(w).Encode(awale)
 }
 
 func check(e error) {
